4cf7df799b3a7137a8a2f3c6f825c9d20c34bc5e
Luisei1120 <cmworks@naver.com>
2024-03-15 11:30:42 +0900

    chp0ex7

--- chp0ex6/chp0ex6.cpp
+++ chp0ex6/chp0ex6.cpp
@@ -2,6 +2,6 @@
 
 #include <iostream>
 
-int main() { { { { { { std::cout << "Hello, World!" << std::endl; }}}}}  return 0; }
+int main() { { { { { { std::cout << "Hello, World!" << std::endl; }}}}}  }
 
 //return0 가 없기 때문에 동작이 불가능합니다.
